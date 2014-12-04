2bec9168ab689524434946b147f4d10fe4ce525c
unknown <s_chernov@pik-n-chernov2.itgroup.local>
2014-12-04 15:42:06 +0300

    Course Project1

--- plot2.R
+++ plot2.R
@@ -1,4 +1,3 @@
-setwd("E:/program/quiz")
 print("start")
 f<-download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","power.zip")
 dataPower<-read.table(unz("power.zip", "household_power_consumption.txt"), 
